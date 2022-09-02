3d043869ed197b20baabfa5eeb3b6560afdb67cb
Wlodzimierz <kowyako@yandex.ru>
2022-09-02 12:14:41 +0200

    Added light/dark themes and Roboto font with start icons

--- DentalClientWithRESTService/MainWindow.xaml.cs
+++ DentalClientWithRESTService/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DentalClientWithRESTService
 {
@@ -20,9 +9,54 @@ namespace DentalClientWithRESTService
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isLightTheme = true;
+
         public MainWindow()
         {
             InitializeComponent();
+            var uri = new Uri("Themes/themeLight.xaml", UriKind.Relative);
+            var resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+
+        /// <summary>
+        /// Draggable header
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void header_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        /// <summary>
+        /// Close app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void closeBtn_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Change app theme
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void changeThemeBtn_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var uri = new Uri(isLightTheme ? "Themes/themeDark.xaml" : "Themes/themeLight.xaml", UriKind.Relative);
+            var resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            changeThemeBtn.Source = isLightTheme ? new BitmapImage(new Uri(@"/Icons/light.png", UriKind.Relative))
+                                                 : new BitmapImage(new Uri(@"/Icons/dark.png", UriKind.Relative));
+            closeBtn.Source = isLightTheme ? new BitmapImage(new Uri(@"/Icons/closeLight.png", UriKind.Relative))
+                                           : new BitmapImage(new Uri(@"/Icons/closeDark.png", UriKind.Relative));
+            isLightTheme = !isLightTheme;
+            
         }
     }
 }
