b07beaa2cc4c2a43d51731557a641936e34abf6c
Wlodzimierz <kowyako@yandex.ru>
2022-09-06 15:43:26 +0200

    Enabled CORS and added logging with NLog

---
+++ RESTDentalService/Services/OperationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTDentalService.Services
+{
+    public interface IOperationService
+    {
+
+    }
+
+    public class OperationService : IOperationService
+    {
+
+    }
+}
