d0a4138c657abac9a241fa90f3fb423a159d7d8e
Wlodzimierz <kowyako@yandex.ru>
2022-09-11 15:03:36 +0200

    Added ClassFixutre for ClinicController

---
+++ RESTDentalService.Tests/ClassFixtures/ClinicControllerFixture.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RESTDentalService.Authorization;
+using RESTDentalService.Controllers;
+using RESTDentalService.Entity;
+using RESTDentalService.Services;
+
+namespace RESTDentalService.Tests.ClassFixtures
+{
+    public class ClinicControllerFixture
+    {
+        public ClinicController Controller;
+        public IClinicService ClinicService;
+        public Mock<DentalRestDbContext> DbMock;
+        public Mock<IMapper> MapperMock;
+        public Mock<ILogger<ClinicService>> LoggerMock;
+        public Mock<IAuthorizationService> AuthServiceMock;
+        public Mock<IUserContextService> UserServiceMock;
+
+        public ClinicControllerFixture()
+        {
+            MapperMock = new Mock<IMapper>();
+            LoggerMock = new Mock<ILogger<ClinicService>>();
+            DbMock = new Mock<DentalRestDbContext>();
+            AuthServiceMock = new Mock<IAuthorizationService>();
+
+            ClinicService = new ClinicService(DbMock.Object,
+                                              MapperMock.Object,
+                                              LoggerMock.Object,
+                                              AuthServiceMock.Object,
+                                              UserServiceMock.Object);
+
+            Controller = new ClinicController(ClinicService);
+        }
+    }
+}
--- RESTDentalService.Tests/ClinicControllerTests.cs
+++ RESTDentalService.Tests/ClinicControllerTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RESTDentalService.Tests.ClassFixtures;
+using Xunit;
 
 namespace RESTDentalService.Tests
 {
-    class ClinicControllerTests
+    public class ClinicControllerTests : IClassFixture<ClinicControllerFixture>
     {
+        private ClinicControllerFixture _target;
+
+        public ClinicControllerTests(ClinicControllerFixture target)
+        {
+            _target = target;
+        }
+
+
     }
 }
