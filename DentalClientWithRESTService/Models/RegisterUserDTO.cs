30d115e6e38429ff3378a5c930ad6369fbf09203
Wlodzimierz <kowyako@yandex.ru>
2022-09-11 11:52:37 +0200

    Worked user registration from client side

---
+++ DentalClientWithRESTService/Models/RegisterUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DentalClientWithRESTService.Models
+{
+    public class RegisterUserDTO
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int RoleId { get; set; } = 3; // User
+    }
+}
--- DentalClientWithRESTService/ViewModels/RegisterViewModel.cs
+++ DentalClientWithRESTService/ViewModels/RegisterViewModel.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DentalClientWithRESTService.HTTPClient;
+using DentalClientWithRESTService.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DentalClientWithRESTService.ViewModels
 {
-    class RegisterViewModel
+    public class RegisterViewModel : INotifyPropertyChanged
     {
+        public RegisterUserDTO UserData { get; set; } = new RegisterUserDTO();
+
+        private string _responseMessage;
+        public string ResponseMessage
+        {
+            get => _responseMessage;
+            private set
+            {
+                _responseMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RelayCommand _registerUser;
+        public RelayCommand RegisterUser =>
+            _registerUser ??= new RelayCommand(async (e) =>
+            {
+                UserData.RoleId++; // bo index Combobox startuje od 0
+                var response = await HttpClientProxy.Instance.Add("account/register", UserData);
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ResponseMessage = "OK";
+                }
+                else
+                {
+                    ResponseMessage = "Rejestracja się nie powiodła";
+                }
+            });
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion
     }
 }
