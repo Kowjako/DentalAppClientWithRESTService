6e6643ef90ee5ab26c73e75a9a5066e508687952
Wlodzimierz <kowyako@yandex.ru>
2022-09-12 08:57:20 +0200

    Added operation fixture and injected to test class

---
+++ RESTDentalService.Tests/ClassFixtures/OperationControllerFixture.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RESTDentalService.Controllers;
+using RESTDentalService.Entity;
+using RESTDentalService.Services;
+
+namespace RESTDentalService.Tests.ClassFixtures
+{
+    public class OperationControllerFixture
+    {
+        public OperationController Controller;
+        public IOperationService Service;
+
+        public Mock<DentalRestDbContext> DbMock;
+        public Mock<IMapper> MapperMock;
+
+        public OperationControllerFixture()
+        {
+            DbMock = new Mock<DentalRestDbContext>();
+            MapperMock = new Mock<IMapper>();
+
+            Service = new OperationService(DbMock.Object, MapperMock.Object, Mock.Of<ILogger<OperationService>>());
+            Controller = new OperationController(Service);
+        }
+    }
+}
--- RESTDentalService.Tests/OperationControllerTests.cs
+++ RESTDentalService.Tests/OperationControllerTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using RESTDentalService.Tests.ClassFixtures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace RESTDentalService.Tests
 {
-    class OperationControllerTests
+    public class OperationControllerTests : IClassFixture<OperationControllerFixture>
     {
+        private OperationControllerFixture _target;
+
+        public OperationControllerTests(OperationControllerFixture target)
+        {
+            _target = target;
+        }
     }
 }
